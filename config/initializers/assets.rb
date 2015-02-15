c917017577d4f11113786ac49c1e610f3a3b14fd
Casey Collins <chcollin@gemini.cs.hamilton.edu>
2015-02-15 14:28:46 -0500

    Commit xml changes, demographic/result storing, and quiz updates

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( slick_quiz/slickQuiz-config1.js )
+Rails.application.config.assets.precompile += %w( slick_quiz/slickQuiz-config2.js )
+Rails.application.config.assets.precompile += %w( slick_quiz/slickQuiz-config3.js )
+Rails.application.config.assets.precompile += %w( slick_quiz/slickQuiz-config4.js )
+Rails.application.config.assets.precompile += %w( slick_quiz/slickQuiz-config5.js )
